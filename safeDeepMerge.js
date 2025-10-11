d0fdc72cf75068e2994f13aa2f0de54740ade59e
Vinícius Ventura <viniciusventura29@gmail.com>
2025-10-11 14:47:44 -0300

    Create safeDeepMerge.js

---
+++ safeDeepMerge.js
@@ -0,0 +1,14 @@
+function safeDeepMerge(target, src) {
+  const banned = ["__proto__", "constructor", "prototype"];
+  for (const key in src) {
+    if (!Object.prototype.hasOwnProperty.call(src, key)) continue;
+    if (banned.includes(key)) continue; // bloqueia chaves perigosas
+    const val = src[key];
+    if (val && typeof val === "object" && !Array.isArray(val)) {
+      if (!target[key] || typeof target[key] !== "object") target[key] = {};
+      safeDeepMerge(target[key], val);
+    } else {
+      target[key] = val;
+    }
+  }
+}
